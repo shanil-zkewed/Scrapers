1155d99e7b72625cbc980ac9def478f5296520fc
shanil-zkewed <shanil@zkewed.com>
2024-04-05 16:25:39 +0530

    update

--- main.py
+++ main.py
@@ -1,20 +1,69 @@
 import os
 import schedule
 import time
+import requests
  
 # running other file using run()
 
 def task():
+    while True:
+        try:
+            os.system("scrapy crawl ikmanvans")
+            break
+        except (requests.exceptions.ConnectionError, FileNotFoundError):
+            print("error, trying again in 10s")
+            time.sleep(5)
+
+        try:
+            os.system("scrapy crawl ikmancars")
+            break
+        except (requests.exceptions.ConnectionError, FileNotFoundError):
+            print("error, trying again in 10s")
+            time.sleep(5)
+
+        try:
+            os.system("scrapy crawl riya_vans")
+            break
+        except (requests.exceptions.ConnectionError, FileNotFoundError):
+            print("error, trying again in 10s")
+            time.sleep(5)
+
+        try:
+            os.system("scrapy crawl riya_suvs")
+            break
+        except (requests.exceptions.ConnectionError, FileNotFoundError):
+            print("error, trying again in 10s")
+            time.sleep(5)
+        
+        
+        try:
+            os.system("scrapy crawl riya_cars")
+            break
+        except (requests.exceptions.ConnectionError, FileNotFoundError):
+            print("error, trying again in 10s")
+            time.sleep(5)
+        
+        try:
+            os.system("scrapy crawl patpat")
+            break
+        except (requests.exceptions.ConnectionError, FileNotFoundError):
+            print("error, trying again in 10s")
+            time.sleep(5)
+        
+
+    '''  
     os.system("scrapy crawl ikmanvans")
     os.system("scrapy crawl ikmancars")
     os.system("scrapy crawl riya_vans")
     os.system("scrapy crawl riya_suvs")
     os.system("scrapy crawl riya_cars")
-    os.system("scrapy crawl patpat")
+    os.system("scrapy crawl patpat")'''
+
+
     print("done!")
 
 
-schedule.every().day.at('07:40:00').do(task)
+schedule.every().day.at('15:05:00').do(task)
 
 while True:
     schedule.run_pending()
